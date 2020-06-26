3f7cbb4a2c798e45944efb57c67eb9f365d9ad46
Vimbayinashe <vimbayinashemandaza@gmail.com>
2020-06-26 23:47:46 +0200

    Add Documentation with POST example.

--- routes/answers.js
+++ routes/answers.js
@@ -8,6 +8,8 @@ router.post('/multiplication', async (req, res) => {
 
         res.status(200).send({ msg: 'Answers received' })
 
+        // send back with id of submitted test (as added to database)?
+
     } catch (error) {
         console.log(error);
         res.status(400).send({ error })
