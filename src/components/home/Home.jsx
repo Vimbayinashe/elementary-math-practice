f51390c8e72ddf68462fef929571a997b42a0115
Vimbayinashe <vimbayinashemandaza@gmail.com>
2020-07-03 19:39:00 +0200

    Add game options buttons to home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -6,7 +6,28 @@ const Home = () => {
     return(
         <main className="home">
             <p>Play and train yourself to become a pro at Maths Master!</p>
+
+            <div className="buttons">
+                <div>
+                    <button>+</button>
+                    <p>Addition</p>
+                </div>
+                <div>
+                    <button>-</button>
+                    <p>Subtraction</p>
+                </div>
+                <div>
+                    <button>&times;</button>
+                    <p>Multiplication</p>
+                </div>
+                <div>
+                    <button>&divide;</button>
+                    <p>Division</p>
+                </div>
+            </div>
+
             <button className="play">Play</button>
+
         </main>
     )
 }
