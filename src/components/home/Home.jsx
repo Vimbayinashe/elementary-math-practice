6800e7cf5e1e2afe22fb1ecd47b643eb78d3a146
Vimbayinashe <vimbayinashemandaza@gmail.com>
2020-07-03 19:01:12 +0200

    Create Home component

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Home = () => {
+
+    return(
+        <main className="home">
+            <p>Play and train yourself to become a pro at Maths Master!</p>
+            <button className="play">Play</button>
+        </main>
+    )
+}
+
+export default Home;
