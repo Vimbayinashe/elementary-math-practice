710897ef7e6e7ae3fcd8e752519e209ee9aba1f1
Vimbayinashe <vimbayinashemandaza@gmail.com>
2020-07-06 14:56:52 +0200

    Remove react-swipe level scroll

--- src/components/game/Level.jsx
+++ src/components/game/Level.jsx
@@ -1,33 +1,19 @@
 import React, { useState } from 'react';
-import ReactSwipe from 'react-swipe';
 import './Level.css';
 
 const Level = () => {
 
     const [level, setLevel] = useState('');
 
-    let reactSwipeEl;
 
     let numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
-    let JSXList = numbers.map( number => (<button onClick={()=>setLevel(number)}> { number } </button>))
+    let JSXList = numbers.map( number => (
+        <button key={number} onClick={()=>setLevel(number)}> { number } </button>))
 
     return(
         <section>
             <h3>Choose Level</h3>
-            <div className = "swipe-level">
-                <ReactSwipe
-                    className="carousel"
-                    swipeOptions={{ continuous: false }}
-                    ref={ el => (reactSwipeEl = el)}
-                    >
-                    <div>easy</div>
-                    <div>medium</div>
-                    <div>hard</div>
-                </ReactSwipe>
-                <button onClick={ () => reactSwipeEl.prev()}>&lt;</button>
-                <button onClick={ () => reactSwipeEl.next()}>&gt;</button>
-            </div>
 
             <article className="level-buttons">
                 {JSXList}
