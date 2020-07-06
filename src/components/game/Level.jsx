52dc9d186ff47eb7187462ffe8d19f32dd733048
Vimbayinashe <vimbayinashemandaza@gmail.com>
2020-07-06 14:42:48 +0200

    Testing react-swipe

--- src/components/game/Level.jsx
+++ src/components/game/Level.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import ReactSwipe from 'react-swipe';
+import './Level.css';
 
 const Level = () => {
 
+    let reactSwipeEl;
+
     return(
         <section>
-            Hi Swipe!
+            <h3>Hi Swipe!</h3>
+            <div className = "swipe-level">
+                <ReactSwipe
+                    className="carousel"
+                    swipeOptions={{ continuous: false }}
+                    ref={ el => (reactSwipeEl = el)}
+                    >
+                    <div>easy</div>
+                    <div>medium</div>
+                    <div>hard</div>
+                </ReactSwipe>
+                <button onClick={ () => reactSwipeEl.prev()}>&lt;</button>
+                <button onClick={ () => reactSwipeEl.next()}>&gt;</button>
+            </div>
+
+            <article>
+                
+            </article>
         </section>
     )
 }
