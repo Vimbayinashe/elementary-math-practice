c4966993c061773bb508d6f13b85fd6b1e5016c0
Vimbayinashe <vimbayinashemandaza@gmail.com>
2020-07-07 18:30:21 +0200

    Test fetch from backend in Play component

--- src/components/game/Play.jsx
+++ src/components/game/Play.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
  import { useHistory } from 'react-router-dom';
 
 const Play = ({ gameMethod, level }) => {
 
     const history = useHistory();
+    const [questions, setQuestions] = useState([]);
 
     console.log('Play: ', gameMethod, level);
 
@@ -11,11 +12,34 @@ const Play = ({ gameMethod, level }) => {
         return history.push(`/game`);
     }
     
+    let url = `http://localhost:3002/questions/${gameMethod}/${level}`;
 
     useEffect(() =>{
         console.log('fetching in Play');
+
+
+        let fetchQuestions = async () => {
+            try {
+                const response = await fetch(url);
+                const json = await response.json();
+
+                if (json) setQuestions(json);
+
+                console.log("questions:");
+                console.log(json);
+                
+                return json;
+
+
+            } catch (error) {
+                console.error(error);
+            }
+        }
+
+        fetchQuestions();
         
-    }, [])
+        
+    }, [url])
     
 
     return(
@@ -24,6 +48,8 @@ const Play = ({ gameMethod, level }) => {
                 (gameMethod && level) ? '' : renderRedirect()
             }
             <h2>Play</h2>
+            <p>Method: { gameMethod } and level: {level}.</p>
+            <p>{questions}</p>
 
         </section>
     )
