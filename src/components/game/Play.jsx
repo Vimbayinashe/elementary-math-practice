530dd4bb39b9765fd23969937d2a13dcb802d634
Vimbayinashe <vimbayinashemandaza@gmail.com>
2020-07-07 18:09:47 +0200

    Redirect to selecting game level from play if gameMethod or Level have not beeen selected

--- src/components/game/Play.jsx
+++ src/components/game/Play.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+ import { useHistory } from 'react-router-dom';
 
 const Play = ({ gameMethod, level }) => {
 
+    const history = useHistory();
+
     console.log('Play: ', gameMethod, level);
+
+    let renderRedirect = () => {
+        return history.push(`/game`);
+    }
+    
+
+    useEffect(() =>{
+        console.log('fetching in Play');
+        
+    }, [])
     
 
     return(
         <section>
+            {
+                (gameMethod && level) ? '' : renderRedirect()
+            }
             <h2>Play</h2>
+
         </section>
     )
 }
