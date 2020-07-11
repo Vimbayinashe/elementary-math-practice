4fced45b5d1e356f050083341728f376fc3a0335
Vimbayinashe <vimbayinashemandaza@gmail.com>
2020-07-11 20:05:17 +0200

    Style and make App header a link

--- src/components/home/Header.jsx
+++ src/components/home/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Menu from './Menu';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 const Header = () => {
@@ -8,8 +8,8 @@ const Header = () => {
     return(
         <header className="App-header">
             <Menu />
-            <h1>Maths App</h1>
-            {/* <h1><Link to="/">Maths App</Link></h1> */}
+            {/* <h1>Maths App</h1> */}
+            <h1><Link to="/">Maths App</Link></h1>
         </header>
     )
 }
